da6dcd3e79f2c4d73df0518124e35a5967acd889
SamB440 <sam@islandearth.net>
2021-12-19 13:44:06 +0000

    Use sendSignChange method

--- src/main/java/com/convallyria/wanderingmarket/util/SignMenuFactory.java
+++ src/main/java/com/convallyria/wanderingmarket/util/SignMenuFactory.java
@@ -3,11 +3,8 @@ package com.convallyria.wanderingmarket.util;
 import com.comphenix.protocol.PacketType;
 import com.comphenix.protocol.ProtocolLibrary;
 import com.comphenix.protocol.events.PacketAdapter;
-import com.comphenix.protocol.events.PacketContainer;
 import com.comphenix.protocol.events.PacketEvent;
 import com.comphenix.protocol.wrappers.BlockPosition;
-import com.comphenix.protocol.wrappers.nbt.NbtCompound;
-import com.comphenix.protocol.wrappers.nbt.NbtFactory;
 import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 import org.bukkit.Location;
@@ -15,7 +12,6 @@ import org.bukkit.Material;
 import org.bukkit.entity.Player;
 import org.bukkit.plugin.Plugin;
 
-import java.lang.reflect.InvocationTargetException;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -23,12 +19,6 @@ import java.util.Objects;
 import java.util.function.BiPredicate;
 
 public final class SignMenuFactory {
-
-    private static final int ACTION_INDEX = 9;
-    private static final int SIGN_LINES = 4;
-
-    private static final String NBT_FORMAT = "{\"text\":\"%s\"}";
-    private static final String NBT_BLOCK_ID = "minecraft:sign";
 
     private final Plugin plugin;
 
@@ -106,32 +96,9 @@ public final class SignMenuFactory {
             this.position = new BlockPosition(location.getBlockX(), location.getBlockY() + (255 - location.getBlockY()), location.getBlockZ());
 
             player.sendBlockChange(this.position.toLocation(location.getWorld()), Material.OAK_SIGN.createBlockData());
+            //todo text colour?
+            player.sendSignChange(this.position.toLocation(location.getWorld()), text.toArray(new String[0]));
 
-            PacketContainer openSign = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.OPEN_SIGN_EDITOR);
-            PacketContainer signData = ProtocolLibrary.getProtocolManager().createPacket(PacketType.Play.Server.TILE_ENTITY_DATA);
-
-            openSign.getBlockPositionModifier().write(0, this.position);
-
-            NbtCompound signNBT = NbtFactory.ofCompound("");
-
-            for (int line = 0; line < SIGN_LINES; line++) {
-                signNBT.put("Text" + (line + 1), this.text.size() > line ? String.format(NBT_FORMAT, color(this.text.get(line))) : "");
-            }
-
-            signNBT.put("x", this.position.getX());
-            signNBT.put("y", this.position.getY());
-            signNBT.put("z", this.position.getZ());
-            signNBT.put("id", NBT_BLOCK_ID);
-
-            signData.getBlockPositionModifier().write(0, this.position);
-            signData.getNbtModifier().write(0, signNBT);
-
-            try {
-                ProtocolLibrary.getProtocolManager().sendServerPacket(player, signData);
-                ProtocolLibrary.getProtocolManager().sendServerPacket(player, openSign);
-            } catch (InvocationTargetException exception) {
-                exception.printStackTrace();
-            }
             inputs.put(player, this);
         }
 
